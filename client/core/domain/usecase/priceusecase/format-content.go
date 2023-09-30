80dfee3bf38d704f2ab71d70281c6363a5817bce
Julio Sandroni <sandronister@gmail.com>
2023-09-30 15:14:05 -0300

    improve formats

--- client/core/domain/usecase/priceusecase/format-content.go
+++ client/core/domain/usecase/priceusecase/format-content.go
@@ -7,5 +7,5 @@ import (
 
 func (u *usecase) formatContent(body []byte) string {
 	current := time.Now()
-	return fmt.Sprintf("Data: %v - Cotacao: %v \n", current.Format("07-09-2006 15:06"), string(body))
+	return fmt.Sprintf("{Data: %v - Dólar: %v }\n", current.Format("07-09-2006 15:06"), string(body))
 }
